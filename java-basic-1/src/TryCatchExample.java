00af013a88c659e4943fdeb23a6aa4ad2a255db0
23fi014 <23fi014@ms.dendai.ac.jp>
2025-05-31 15:52:13 +0900

    change

--- java-basic-1/src/TryCatchExample.java
+++ java-basic-1/src/TryCatchExample.java
@@ -2,7 +2,7 @@ public class TryCatchExample {
     public static void main(String[] args) {
         try {
             // 例外が発生する可能性のあるコードを記述
-            int[] numbers = {1, 2, 3};
+            int[] numbers = {1, 2, 4};
             System.out.println(numbers[4]); // 配列の範囲外にアクセス
         } catch (ArrayIndexOutOfBoundsException e) {
             // 例外が発生した場合の処理を記述
